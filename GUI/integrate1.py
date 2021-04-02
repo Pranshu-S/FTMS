30a2183a56085c2e52147562f2bf545b007a2138
Yuvasankar B <yuvasankar.b@gmail.com>
2021-04-03 00:19:42 +0530

    integration80
    integration80

--- GUI/integrate1.py
+++ GUI/integrate1.py
@@ -32,8 +32,34 @@ class Login:
 
         forget=Button(Frame_login, text="Forget Password?",bg="white",fg="#fc6203",bd=0, font=("Sans Serif",10)).place(x=100, y= 260)
         new_user=Button(Frame_login, text="Register",bg="white",fg="#fc6203", bd=0, font=("Sans Serif",10),command=open).place(x=280, y= 260)
+        def check():
+            conn=sqlite3.connect('FTMS.db')
+            crsr=conn.cursor()
+            crsr.execute("SELECT * FROM FARMER WHERE F_ID=:USER_ID",
+            {
+                'USER_ID':self.txt_user.get()
+            })
+            c=crsr.fetchall()
+            if len(c) != 0:
+                if c[0][1]==self.txt_pass.get():
+                    top2=Toplevel()
+                    obj3=FPortal(top2)
+            else:
+                crsr.execute("SELECT * FROM BUYER WHERE B_ID=:USER_ID",
+                {
+                'USER_ID':self.txt_user.get()
+                })
+                c=crsr.fetchall()
+                if c != 0:
+                    if c[0][1]==self.txt_pass.get():
+                        top2=Toplevel()
+                        obj3=BPortal(top2)
 
-        Login_Button=Button(self.root, text="Login",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=640, y= 420)
+
+            conn.commit()
+            conn.close()
+
+        Login_Button=Button(self.root, text="Login",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20),command=check).place(x=640, y= 420)
 class Register:
     def __init__(self,root):
         self.root = root
@@ -60,6 +86,7 @@ class Register:
 
         Next_Button =Button(self.root, text="Next",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20),command = lambda: clicked(var.get())).place(x=640, y= 420)
         def clicked(value):
+            global top
             top=Toplevel()
             if value==1:
                 obj2=Register_F(top)
@@ -124,8 +151,10 @@ class Register_F:
                 })
             conn.commit()
             conn.close()
+            top.destroy()
+            top1.destroy()
         
-        Next_Button =Button(self.root, text="Next",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20),command = submit).place(x=640, y= 420)
+        Next_Button =Button(self.root, text="Register",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20),command = submit).place(x=640, y= 420)
         
 class Register_B:
     def __init__(self,root):
@@ -169,13 +198,77 @@ class Register_B:
         lbl_contact = Label(Frame_login,text="Contact Number",font=("Sans Serif",15),fg="#fc6203", bg="white").place(x=400, y=110)
         self.txt_contact = Entry(Frame_login, font=("Sans Serif",10),bg="#ebedf0")
         self.txt_contact.place(x=400,y=140, width=250, height=35)
+        def submit():
+            conn=sqlite3.connect('FTMS.db')
+            crsr=conn.cursor()
+            crsr.execute("INSERT INTO BUYER VALUES(:USER_ID,:PWD, :NAME, :LOC ,:CONTACT)",
+                {
+                    'USER_ID':self.txt_user.get(),
+                    'PWD':self.txt_pass.get(),
+                    'NAME':self.txt_name.get(),
+                    'LOC':self.txt_location.get(),
+                    'CONTACT':self.txt_contact.get()
+                })
+            conn.commit()
+            conn.close()
+            top.destroy()
+            top1.destroy()
+        Next_Button =Button(self.root, text="Register",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20),command=submit).place(x=640, y= 420)           
+class FPortal:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("FARMER PORTAL")
+        self.root.geometry("1024x640")
+        self.root.resizable(False,False)
+        
+        # Background Image
+        self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+        self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+        # Login Frames
+        Frame_login=Frame(self.root, bg="white")
+        Frame_login.place(x=50, y=50, height=550, width=900)
 
-        Next_Button =Button(self.root, text="Next",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=640, y= 420)           
+        title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+        title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+        desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+        view_quotations = Button(Frame_login,text="View Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=110)
 
+        show_history = Button(Frame_login,text="Show History",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=190)
+
+        edit_profile = Button(Frame_login,text="Edit Profile",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=260)
+class BPortal:
+        def __init__(self,root):
+            self.root = root
+            self.root.title("BUYER PORTAL")
+            self.root.geometry("1024x640")
+            self.root.resizable(False,False)
+        
+            # Background Image
+            self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+            self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+            # Login Frames
+            Frame_login=Frame(self.root, bg="white")
+            Frame_login.place(x=50, y=50, height=550, width=900)
+
+            title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+            title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+            desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+            Make_quotations = Button(Frame_login,text="Make Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=110)
+
+            show_history = Button(Frame_login,text="Show History",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=190)
+
+            edit_profile = Button(Frame_login,text="Edit Profile",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=260)
+
+            my_quotations = Button(Frame_login,text="My Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=330)
 
 def open():
-    top=Toplevel()
-    obj1=Register(top)
+    global top1
+    top1=Toplevel()
+    obj1=Register(top1)
 root=Tk()
 obj = Login(root)
 root.mainloop()
