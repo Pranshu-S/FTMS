62ea1a9936e21ceb201726c6d1e80aba90a85f7d
Pranshu-S <pranshushukla.191me260@nitk.edu.in>
2021-04-03 23:54:47 +0530

    All Pages in Buyer Portal

---
+++ GUI/Show-History-buyer.py
@@ -0,0 +1,59 @@
+# Import Tkinter Module
+from tkinter import *
+
+from PIL import ImageTk
+
+# Create Login Class
+class Register:
+    def __init__(self,root):
+
+        self.root = root
+        self.root.title("Register User")
+        self.root.geometry("1024x640")
+        self.root.resizable(False,False)
+        
+        # Background Image
+        self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+        self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+        # Login Frames
+        Frame_login=Frame(self.root, bg="white")
+        Frame_login.place(x=50, y=50, height=550, width=900)
+
+        title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+        title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+        desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+        self.all_quotes = Listbox(Frame_login,bg="white",fg="#fc6203", bd=0,height=14,width=100)
+        self.all_quotes.place(x=50,y = 100)
+        # self.all_quotes.pack()
+
+
+        print(type(self.all_quotes))
+        # self.all_quotes = Listbox(Frame_login)
+        my_list = [100,200,300,400,500]
+
+        for item in my_list:
+            self.all_quotes.insert(END, item)
+
+        More_Details = Button(Frame_login,text="Show Details",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0, command = self.select).place(x=70, y=400)
+        Back = Button(Frame_login,text="Back",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=500)
+
+        self.display_label = Label(Frame_login,text='')
+        self.display_label.place(x=400, y=400)
+
+    def select(self):
+        self.display_label.config(text=self.all_quotes.get(ANCHOR))
+
+# Create Tkinter object
+root = Tk()
+
+# Make Login object
+obj = Register(root)
+
+root.mainloop()
+
+
+# Register - Ask if farmer/buyer
+# Farmer - USERID, PAss, name, location, contact number,  (if farmer/buyer), Which crops (checklist)
+# Buyer - USERID, PAss, name, location, contact number,  (if farmer/buyer),
--- GUI/integrate1.py
+++ GUI/integrate1.py
@@ -271,12 +271,22 @@ class BPortal:
         desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=130, y=70) 
         Make_quotations = Button(Frame_login,text="Make Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0, width=20, command=self.make).place(x=110, y=120)
         show_history = Button(Frame_login,text="Show History",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0, width=20).place(x=110, y=180)
-        edit_profile = Button(Frame_login,text="Edit Profile",font=("Sans Serif",15),bg="#fc6203",fg="white",width=20, bd=0).place(x=110, y=240)
-        my_quotations = Button(Frame_login,text="My Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", width=20,bd=0).place(x=110, y=300)
+        edit_profile = Button(Frame_login,text="Edit Profile",font=("Sans Serif",15),bg="#fc6203",fg="white",width=20, bd=0, command=self.edit_b).place(x=110, y=240)
+        my_quotations = Button(Frame_login,text="My Quotations",font=("Sans Serif",15),bg="#fc6203",fg="white", width=20,bd=0, command=self.my_q).place(x=110, y=300)
 
     def make(self):
         Make_quote = Make_Q(self.root)
         root.mainloop()
+
+    def my_q(self):
+        My_quotation = My_Q(self.root)
+
+    def edit_b(self):
+        edit_buyer = Edit_B(self.root)
+        root.mainloop()
+
+    def show_history_b(self):
+        hist = Show_H_B(self.root)
 
 class Show_Q:
     def __init__(self,root):
@@ -362,7 +372,6 @@ class Show_H:
         back=FPortal(self.root)    
     def select(self):
         self.display_label.config(text=self.all_quotes.get(ANCHOR))
-
 
 class Edit_F:
     global ID
@@ -464,9 +473,127 @@ class Make_Q:
         Submit_Button=Button(self.root, text="Submit",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=680, y= 420)
         Back_Button=Button(self.root, text="Back",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=550, y= 420)
 
+class My_Q:
+    def __init__(self,root):
+
+        self.root = root
+        self.root.title("Register User")
+        self.root.geometry("1024x640")
+        self.root.resizable(False,False)
+        
+        # Background Image
+        self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+        self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+        # Login Frames
+        Frame_login=Frame(self.root, bg="white")
+        Frame_login.place(x=50, y=50, height=550, width=900)
+
+        title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+        title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+        desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+        self.all_quotes = Listbox(Frame_login,bg="white",fg="#fc6203", bd=0,height=14,width=100)
+        self.all_quotes.place(x=50,y = 100)
+        # self.all_quotes.pack()
 
 
-    
+        print(type(self.all_quotes))
+        # self.all_quotes = Listbox(Frame_login)
+        my_list = [100,200,300,400,500]
+
+        for item in my_list:
+            self.all_quotes.insert(END, item)
+
+        More_Details = Button(Frame_login,text="Show Details",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0, command = self.select).place(x=70, y=400)
+        Back = Button(Frame_login,text="Back",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=500)
+
+        self.display_label = Label(Frame_login,text='')
+        self.display_label.place(x=400, y=400)
+
+    def select(self):
+        self.display_label.config(text=self.all_quotes.get(ANCHOR))
+
+class Edit_B:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Register User")
+        self.root.geometry("1024x640")
+        self.root.resizable(False,False)
+        
+        # Background Image
+        self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+        self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+        # Login Frames
+        Frame_login=Frame(self.root, bg="white")
+        Frame_login.place(x=170, y=100, height=500, width=700)
+
+        title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+        title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+        desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+
+        lbl_pass = Label(Frame_login,text="Change Password",font=("Sans Serif",15),fg="#fc6203", bg="white").place(x=70, y=190)
+        self.txt_pass = Entry(Frame_login, font=("Sans Serif",10),bg="#ebedf0")
+        self.txt_pass.place(x=70,y=220, width=250, height=35)
+
+        conf_lbl_pass = Label(Frame_login,text="Confirm Change Password",font=("Sans Serif",15),fg="#fc6203", bg="white").place(x=400, y=190)
+        self.txt_conf_pass = Entry(Frame_login, font=("Sans Serif",10),bg="#ebedf0")
+        self.txt_conf_pass.place(x=400,y=220, width=250, height=35)
+
+        lbl_location = Label(Frame_login,text="Change Location",font=("Sans Serif",15),fg="#fc6203", bg="white").place(x=400, y=110)
+        self.txt_location = Entry(Frame_login, font=("Sans Serif",10),bg="#ebedf0")
+        self.txt_location.place(x=400,y=140, width=250, height=35)
+
+        lbl_contact = Label(Frame_login,text="Change Contact Number",font=("Sans Serif",15),fg="#fc6203", bg="white").place(x=70, y=110)
+        self.txt_contact = Entry(Frame_login, font=("Sans Serif",10),bg="#ebedf0")
+        self.txt_contact.place(x=70,y=140, width=250, height=35)
+
+        Save =Button(self.root, text="Save",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=640, y= 420)
+        Back =Button(self.root, text="Back",bg="#fc6203",fg="white", bd=0, font=("Sans Serif",20)).place(x=640, y= 500)
+
+class Show_H_B:
+    def __init__(self,root):
+
+        self.root = root
+        self.root.title("Register User")
+        self.root.geometry("1024x640")
+        self.root.resizable(False,False)
+        
+        # Background Image
+        self.bg =  ImageTk.PhotoImage(file="Login_img.jpg")
+        self.bg_image = Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1 )
+        
+        # Login Frames
+        Frame_login=Frame(self.root, bg="white")
+        Frame_login.place(x=50, y=50, height=550, width=900)
+
+        title = Label(Frame_login, text="Register on", font=("Sans Serif",20),fg="black", bg="white").place(x=170, y=30)
+        title_2 = Label(Frame_login, text="FTMS ", font=("Sans Serif",20, "bold"),fg="#fc6203", bg="white").place(x=330, y=30)
+        desc = Label(Frame_login, text="Farmers Transaction Management System ", font=("Sans Serif",10),fg="grey", bg="white").place(x=115, y=70)
+        
+        self.all_quotes = Listbox(Frame_login,bg="white",fg="#fc6203", bd=0,height=14,width=100)
+        self.all_quotes.place(x=50,y = 100)
+        # self.all_quotes.pack()
+
+        print(type(self.all_quotes))
+        # self.all_quotes = Listbox(Frame_login)
+        my_list = [100,200,300,400,500]
+
+        for item in my_list:
+            self.all_quotes.insert(END, item)
+
+        More_Details = Button(Frame_login,text="Show Details",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0, command = self.select).place(x=70, y=400)
+        Back = Button(Frame_login,text="Back",font=("Sans Serif",15),bg="#fc6203",fg="white", bd=0).place(x=70, y=500)
+
+        self.display_label = Label(Frame_login,text='')
+        self.display_label.place(x=400, y=400)
+
+    def select(self):
+        self.display_label.config(text=self.all_quotes.get(ANCHOR))
+
+
 global root
 root=Tk()
 obj = Login(root)
